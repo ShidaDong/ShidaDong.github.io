1c2e9a23a652ceb19d463c5d7ade188d1a07deff
ShidaDong <1746688933@qq.com>
2023-12-14 19:15:11 +0800

    upload website
    upload website

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+/*
+    Student Name: Shida Dong
+    File Name: script.js
+    Date: 12-12-2023
+*/
+
+//Hamburger menu function
+
+function hamburger() {
+    var menu = document.getElementById('menu-links');
+    var logo = document.getElementById('ffc-logo');
+    if (menu.style.display === 'block' && logo.style.display === 'none') {
+        menu.style.display = 'none';
+        logo.style.display = 'block';
+    } else {
+        menu.style.display = 'block';
+        logo.style.display = 'none';
+    }
+}
